56658966f5a139233d3b5238870147c2581ab51d
kodir-zaelani <kodir.zaelani78@gmail.com>
2025-12-06 00:01:18 +0800

    traits format

--- app/Traits/HasFormatRupiah.php
+++ app/Traits/HasFormatRupiah.php
@@ -5,9 +5,8 @@ namespace App\Traits;
 trait HasFormatRupiah
 {
     function formatRupiah($field, $prefix = null)
-{
-    $prefix = $prefix ? $prefix : 'Rp. ';
-    $nominal = $this->attributes[$field];
-    return $prefix . number_format($nominal, 0, ',', '.');
-}
+    {
+        $prefix = $prefix ? $prefix : 'Rp. ';
+        return $prefix . number_format($this->attributes[$field], 0, ',', '.');
+    }
 }
