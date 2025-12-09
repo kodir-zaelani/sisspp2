1939baae3e6876d1306c27e20b1a15b50a558113
kodir-zaelani <kodir.zaelani78@gmail.com>
2025-12-09 19:21:43 +0800

    update wali pesertadidik

--- resources/views/backend/partials/header-horizontal.blade.php
+++ resources/views/backend/partials/header-horizontal.blade.php
@@ -22,7 +22,7 @@
         </div>
         <nav class="navbar navbar-static-top">
             <div class="app-menu">
-                <h3>{{strtoupper($datapeseradidik_global->nama)}} | {{$datapeseradidik_global->nisn}}</h3>
+                {{-- <h3>{{strtoupper($datapeseradidik_global->nama)}} | {{$datapeseradidik_global->nisn}}</h3> --}}
                 <ul class="header-megamenu nav">
 
                 </ul>
