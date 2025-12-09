3d2b5d2823586462b41838bab6d63470c4a67944
kodir-zaelani <kodir.zaelani78@gmail.com>
2025-12-09 19:27:00 +0800

    update wali pesertadidik

--- resources/views/wali/home/index.blade.php
+++ resources/views/wali/home/index.blade.php
@@ -26,7 +26,7 @@
                     <h4 class="box-title">Peserta Didik</h4>
                 </div>
                 <div class="box-body">
-                {{-- <h3>{{$datapeseradidik_global->nama}} | {{$datapeseradidik_global->nisn}}</h3> --}}
+                <h3>{{$datapeseradidik->nama}} | {{$datapeseradidik->nisn}}</h3>
                     <p>{{$datapeseradidik->sekolah->nama}}</p>
                 </div>
                 <!-- /.box-body -->
