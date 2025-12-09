127d080ccf459886bb9e178157b0f7441b2ffb04
kodir-zaelani <kodir.zaelani78@gmail.com>
2025-12-09 19:22:50 +0800

    update wali pesertadidik

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\ManajemenPanelProvider::class,
-    App\Providers\ViewAuthwaliServiceProvider::class,
+    // App\Providers\ViewAuthwaliServiceProvider::class,
     App\Providers\ViewSettingServiceProvider::class,
 ];
